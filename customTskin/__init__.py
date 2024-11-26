737084abea807c0486a8f56df055c2f59d84aa49
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-11-26 12:43:49 +0100

    feat : Added methods for prediction evaluation

--- customTskin/__init__.py
+++ customTskin/__init__.py
@@ -1,9 +1,8 @@
 import time
-
+from .utils import most_common
 from multiprocessing import Pipe
 from tactigon_gear import Ble, TSkinConfig, Hand, OneFingerGesture, TwoFingerGesture
 from typing import Optional
-
 from .middleware import ITSGesture
 
 class CustomTskin(Ble):
@@ -14,8 +13,9 @@ class CustomTskin(Ble):
 
         sensor_rx, self._sensor_tx = Pipe(duplex=False)
         audio_rx, self._audio_tx = Pipe(duplex=False)
+        self._action_pipe, action_pipe = Pipe(duplex=False)
 
-        self.middleware = ITSGesture(sensor_rx, audio_rx)
+        self.middleware = ITSGesture(sensor_rx, audio_rx,action_pipe)
         self.middleware.can_run.set()
 
     def start(self):
@@ -26,3 +26,10 @@ class CustomTskin(Ble):
         Ble.join(self, timeout)
         self.middleware.can_run.clear()
         self.middleware.join(timeout)
+
+    def evaluate_move(self):
+        action = []
+        while self._action_pipe.poll(timeout=0.02):
+            action.append(self._action_pipe.recv())
+        return most_common(action)
+        
